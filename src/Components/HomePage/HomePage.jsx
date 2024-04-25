44aaaa9c4cf9a0bad4b334da9adce83729ee1d1b
claudia <ceruleancee@gmail.com>
2024-04-24 20:56:17 -0500

    Add navigation

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -7,7 +7,8 @@ const HomePage = () => {
     <Grid container xs={12} className='px-5 lg:px-36 justify-between'>
         
         <Grid item xs={0} lg={2.5} className='hidden lg:block w-full relative'>
-        <p className='text-center'>left part</p>
+        {/* <p className='text-center'>left part</p> */}
+       <Navigation/>
         </Grid>
 
         <Grid item xs={12} lg={6} className='hidden lg:block w-full relative'>
---
+++ src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div className='h-screen sticky top-0'>
+        <div>
+            <div className='py-5'>
+                <img src="" alt=""/>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navigation
